525cc74daad751e8ced913c03153fc637c30da66
aknightdev <developer@knightagency.com.au>
2022-10-12 14:41:09 +0530

    Updated on 12-10-2022

--- src-08-10/components/context.js
+++
@@ -1,4 +0,0 @@
-import { createContext } from 'react';
-export const formDetailContext = createContext();
-export const formHealthContext = createContext();
-export const formEbookContext = createContext();
