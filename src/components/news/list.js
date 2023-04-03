58e25c914920e106e73853d73d0c76dd80678fac
aknightdev <developer@knightagency.com.au>
2023-04-03 16:34:09 +0530

    anges

--- src/components/news/list.js
+++ src/components/news/list.js
@@ -1,11 +1,78 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-const NewsList = (props) => (
-  <section className="news-list">
+const NewsList = (props) => {
+  const [curCat, setCurCat] = React.useState('');
+  const [showCat, setShowCat] = React.useState(false);
+  const [showFilter, setShowFilter] = React.useState(false);
+  let whyMG = [];let cats=[]; let cnames=[];
+  props.data.map((d) => {
+    let catsd= [];
+    if(d.newscategories){
+      d.newscategories.nodes.map((d1) => {
+        if(cats.indexOf(d1.id)==-1){
+          cats.push(d1.id);
+          cnames.push(d1);
+        }
+        if(catsd.indexOf(d1.id)==-1){
+          catsd.push(d1.id);
+        }
+      });
+    }
+    d['cats'] = catsd;
+    return whyMG.push(d);
+  });
+  cnames.sort((a, b) => {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  });
+  const [filterList, setFilterList] = React.useState(whyMG);
+  const fiterNews = (v) =>{
+    setCurCat(v);
+    let whyMG1 = whyMG;
+    if(v!=''){
+      whyMG1 = whyMG1.filter((d) => {
+        return d.cats.indexOf(v)>-1;
+      });
+    }
+    setFilterList(whyMG1);
+  }
+
+  React.useEffect(() => {
+     if(window.location.href.includes('news')){
+      setShowFilter(true);
+     }
+     return () => {
+      setShowFilter(false);
+     }
+   }, []);
+
+  return (<section className="news-list allnews_list">
     <div className="container">
+    {showFilter?
+      <div className="ccopportun">
+        <div class="row">
+          <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+            <h3 className={showCat?"activeloc":""} onClick={()=>{setShowCat(!showCat);setCurCat('');}}>Categories</h3>
+            <div className={showCat?"activeloc job_list":"job_list"}>                     
+              <ul>
+              <li className={curCat==''?'active':''} onClick={()=>fiterNews('')}>All</li>
+                {cnames.map((d, index) => {
+                 return <li className={curCat==d.id?'active':''} onClick={()=>fiterNews(d.id)}>{d.name}</li>
+                })}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>:null}      
+
       <div className="row">
-        {props.data.map((d, index) => {
+        {filterList.map((d, index) => {
           return <div className="col-lg-4 col-md-12  mt-4">
             <div className="listbg">
               <img className="img-fluid" style={{ objectFit: "cover" }} src={d.featuredImage?.node?.mediaItemUrl} alt={d.featuredImage?.node?.altText} />
@@ -20,7 +87,7 @@ const NewsList = (props) => (
         <Link className="bt-big" to="/news/" role="button">All News &amp; Articles</Link>
       </div> : ""}
     </div>
-  </section>
-)
+  </section>)
+}
 
 export default NewsList
