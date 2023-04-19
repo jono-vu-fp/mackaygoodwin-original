96bae09603e63012c440c7d979b440e06960f236
aknightdev <102014903+aknightdev@users.noreply.github.com>
2023-04-19 14:24:48 +0530

    Update html.js

--- src/html.js
+++ src/html.js
@@ -5,16 +5,7 @@ export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
       <head>
-        {/* Google Tag Manager */}
-        <script dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PRCLGBS');`}}
-        />
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PRCLGBS" height="0" width="0" style={{ "display": "none", "visibility": "hidden" }}></iframe></noscript>
-        {/* Google Tag Manager End */}
+        
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
