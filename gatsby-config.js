8dd56dd61093776f730bb55dce33841803ff3246
aknightdev <developer@knightagency.com.au>
2023-08-08 10:34:52 +0530

    New changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,15 +48,15 @@ module.exports = {
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+      // options: {
+      //   name: `gatsby-starter-default`,
+      //   short_name: `starter`,
+      //   start_url: `/`,
+      //   background_color: `#663399`,
+      //   theme_color: `#663399`,
+      //   display: `minimal-ui`,
+      //   icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      // },
     },
     `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-styled-components`,
