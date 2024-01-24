261108b92fc4a37954396b9217ca802fe8a1d82a
aknightdev <developer@knightagency.com.au>
2024-01-24 14:32:21 +0530

    pushed by knight agency at 24-01-2024-14-32

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,19 +45,7 @@ module.exports = {
       }
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+    
     `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-force-trailing-slashes`,
